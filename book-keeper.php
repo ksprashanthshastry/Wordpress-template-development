7236ae28a008a1fd04110a8611c2760fbc250c13
ksprashanthshastry <ksprashanthshastry@gmail.com>
2021-01-17 19:30:34 +1000

    boiler plate template for plugin

---
+++ book-keeper.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name:       Book Keeper
+ * Plugin URI:
+ * Description:       Book Keeper allows you to add new books to the list of books, edit the book information and delete the book entry if its no more needed.
+ * Version:           1.0.0
+ * Author:            Prashanth Shastry
+ * Author URI:
+ */
+
